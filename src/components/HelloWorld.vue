4ae9685fdeeda44e323ed1b2a8e9134c1dfd53e1
Kevin Pavon <kevin0tw2@gmail.com>
2021-02-14 17:21:55 -0600

    Ya final no voy a hacer más

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,54 @@
 <template>
-  <div class="p-2 m-2 col-span-3 md:col-span-2">
-    <div v-for="post in posts" :key="post.id" class="bg-white rounded mb-4 grid grid-cols-3 gap-2">
-      <a :href="post.url" target="_blank" rel="noopener noreferrer">
-        <img :src="post.content | onlyImage" alt="" class="float-left rounded-l">
+  <div class="p-2 m-2 col-span-3">
+    <div
+      v-for="post in posts"
+      :key="post.id"
+      class="mb-4 grid grid-cols-3 gap-2"
+    >
+      <a
+        :href="post.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="col-span-3 md:col-span-1 mx-auto"
+      >
+        <img
+          :src="post.content | onlyImage"
+          alt=""
+          class="float-left rounded"
+        />
       </a>
-      
-      <div class="p-2 col-span-2">
-        <h1 class="text-base md:text-lg"><a :href="post.url" target="_blank" rel="noopener noreferrer">{{post.title}}</a>end
-        
+      <div class="bg-white rounded p-2 col-span-3 md:col-span-1 flex flex-col">
+        <h1 class="text-base md:text-lg font-bold text-purple-700">
+          <a :href="post.url" target="_blank" rel="noopener noreferrer">{{
+            post.title
+          }}</a
+          >end
         </h1>
-        {{post.content | insidePTag}}
+        <p class="flex-auto">{{ post.content | insidePTag }}</p>
+        <p>
+          Publicado por
+          <a
+            :href="post.author.url"
+            class="text-purple-700 transition duration-300 hover:text-indigo-300"
+            >{{ post.author.displayName }}</a
+          ><br />
+        </p>
+        <p class="label">
+          <a
+            v-for="label in post.labels"
+            :key="label"
+            class="text-purple-700 transition duration-100 hover:text-indigo-300"
+            :href="`https://www.patyscans.com/search/label/${label}`"
+            >{{ label }}</a
+          >
+        </p>
+      </div>
+      <div
+        class="bg-gradient-to-b from-green-400 to-purple-500 rounded p-2 col-span-1 hidden md:block"
+      >
+        <span class="text-9xl flex h-full justify-center items-center">{{
+          post.replies.totalItems
+        }}</span>
       </div>
     </div>
   </div>
@@ -22,18 +61,23 @@ export default {
     posts: Array
   },
   filters: {
-    onlyImage: function (value) {
-      if (!value) return ''
-      value = value.toString()
-      return value.match(/https.+?\.(png|jpg)/)[0]
+    onlyImage: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.match(/https.+?\.(png|jpg)/)[0];
     },
-    insidePTag: function (value) {
-      if (!value) return ''
-      return value.match(/<p>(.*?)<\/p>/)[0].replace(/(<[^>]*>?|&nbsp;)/g, '');
-    },
+    insidePTag: function(value) {
+      if (!value) return "";
+      return value.match(/<p>(.*?)<\/p>/)[0].replace(/(<[^>]*>?|&nbsp;)/g, "");
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.label a:not(:last-child):after {
+  color: navy;
+  content: ", ";
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,138 @@
+<template>
+  <div v-on:click="changeHeader" class="container  mx-auto mb-4">
+    <img :src="pajaro" alt="" class="w-full" />
+    <div id="nav" class="bg-white rounded-b flex flex-col">
+      <button class="md:hidden" v-on:click="isNav = !isNav">x</button>
+      <ul
+        class="active md:flex flex-col md:flex-row justify-center w-full overflow-visible"
+        v-bind:class="{ hidden: isNav }"
+      >
+        <li class="flex-auto text-center">
+          <router-link to="/" class="font-sans transition duration-100"
+            ><img
+              alt="Vue logo"
+              src="@/assets/logo.png"
+              class="inline-block w-6"
+          /></router-link>
+        </li>
+        <li class="flex-auto text-center">
+          <router-link to="#" class="font-sans transition duration-100"
+            >Guías ▶</router-link
+          >
+          <ul>
+            <li><a href="#">Edición básica</a></li>
+            <li><a href="#">Fuentes</a></li>
+            <li><a href="#">Cómo escribir bien</a></li>
+            <li><a href="#">Consideraciones de traducción</a></li>
+          </ul>
+        </li>
+        <li class="flex-auto text-center">
+          <a href="#" class="font-sans transition duration-100"
+            >Reclutamiento</a
+          >
+        </li>
+        <li class="flex-auto text-center">
+          <a href="#" class="font-sans transition duration-100">Lector</a>
+        </li>
+        <li class="flex-auto text-center">
+          <a href="#" class="font-sans transition duration-100">Pedidos</a>
+        </li>
+        <li class="flex-auto text-center">
+          <a href="#" class="font-sans transition duration-100">Proyectos ▶</a>
+          <ul>
+            <li>
+              <a href="#">Touhou ▶</a>
+              <ul>
+                <li><a href="#">Activos</a></li>
+                <li><a href="#">Finalizados</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">No Touhou ▶</a>
+              <ul>
+                <li><a href="#">Activos</a></li>
+                <li><a href="#">Finalizados</a></li>
+                <li><a href="#">Pausados</a></li>
+                <li><a href="#">Abandonados</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li class="flex-auto text-center">
+          <a href="#" class="font-sans transition duration-100"
+            >Páginas recomendadas</a
+          >
+        </li>
+        <li class="flex-auto text-center">
+          <a href="#" class="font-sans transition duration-100"
+            >Info del Scan ▶</a
+          >
+          <ul>
+            <li><a href="#">Sobre nosotros</a></li>
+            <li><a href="#">Sobre Touhou</a></li>
+            <li><a href="#">Staff</a></li>
+            <li><a href="#">Discord</a></li>
+          </ul>
+        </li>
+        <li class="flex-auto text-center">
+          <a href="#" class="font-sans transition duration-100">+18</a>
+        </li>
+        <li class="flex-auto text-center">
+          <a href="#" class="font-sans transition duration-100">♥</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      parvada: [
+        "https://1.bp.blogspot.com/-uZ-mBSDDy-4/VDaK7gqJqdI/AAAAAAAAAr4/BmK_wY8-ppsl2AyMudTJ9emF9YtvUE1yQCPcBGAYYCw/s1600/portada.png",
+        "https://1.bp.blogspot.com/-MOHUHg7lLRw/W7VYDhemUNI/AAAAAAAADYk/DT9dTPLRSBM5NHb8cGXzN0Ht4yO7ccWCACPcBGAYYCw/s1600/banner1.png",
+        "https://1.bp.blogspot.com/-2CTSJDDYyME/XlhPDFT_iSI/AAAAAAAAFg0/0-UrxW9gj8cTprMI_4IzZm4FHeVycRmLACLcBGAsYHQ/s1600/alic.jpg",
+        "https://1.bp.blogspot.com/-ASB7xHf4KHY/XlhPC4BYMdI/AAAAAAAAFgw/GKRngEPwfoseLLCnE-F-qHbPtUWVtFK3gCLcBGAsYHQ/s1600/ayaya.jpg",
+        "https://1.bp.blogspot.com/-CkugRa2qoZY/XlhPDuvjZ7I/AAAAAAAAFg8/gYGO3V-ewZ4qOa5zWmV9l9Ei7uWukLSogCLcBGAsYHQ/s1600/banner-1.jpg"
+        // ,'https://1.bp.blogspot.com/-fRIrHEECRBM/XlhPD_NBNuI/AAAAAAAAFhA/QpzNrvacWN0JkpcfUfLV4ht27_uMu6z3gCLcBGAsYHQ/s1600/banner-2.jpg'
+        // ,'https://1.bp.blogspot.com/-F9WQCob5AAA/XlhPEqHBoeI/AAAAAAAAFhE/nO-YTDv5VC4LhQAEnWacRSecnGhMZ-lDwCLcBGAsYHQ/s1600/banner-3.jpg'
+        // ,'https://1.bp.blogspot.com/-afmDDIo3wwU/XlhPEo95n3I/AAAAAAAAFhI/qqQvlUwzR4UlONFPygTklvdQEovppatLgCLcBGAsYHQ/s1600/banner-4.jpg'
+        // ,'https://1.bp.blogspot.com/-8S7znS1bpkQ/XlhPFl8UI3I/AAAAAAAAFhQ/3NK7CruZYYwsG75GfcRtNE9zQhF2ej6hACLcBGAsYHQ/s1600/banner-6.jpg'
+        // ,'https://1.bp.blogspot.com/-v2YqI7S7s9A/XlhPF7RCpqI/AAAAAAAAFhY/9OL06YmrY_QJuU0KijNuKPQGzgZ3rYDyQCLcBGAsYHQ/s1600/gabe.jpg'
+        // ,'https://1.bp.blogspot.com/-9ADisILLXhw/XlhPGf9SngI/AAAAAAAAFhc/Ag6Ss1ce8GAAPZO0DlqJlHW7ajTWzGNegCLcBGAsYHQ/s1600/joimu.png'
+        // ,'https://1.bp.blogspot.com/-eEKAGmFt1mk/XlhPGokfHQI/AAAAAAAAFhg/_3SpptJ1QJ8yMZfR7uTjIRQm2n5f1bq0wCLcBGAsYHQ/s1600/lameso.png'
+        // ,'https://1.bp.blogspot.com/-ml-IUpYAml0/XlhPDN0CQBI/AAAAAAAAFg4/InmQXbGI50gyKT2Khwc4GEzirBrnMiu2wCLcBGAsYHQ/s1600/Marisa.png'
+        // ,'https://1.bp.blogspot.com/-CbUeuXM4PPE/XlhPG6vOW5I/AAAAAAAAFhk/qVk0NPK1mNo0s9om77YUHqOLXHuqZF_7gCLcBGAsYHQ/s1600/oldpaty.png'
+        // ,'https://1.bp.blogspot.com/-8hKxpN1v8vo/XlhPHPrU0CI/AAAAAAAAFho/nr4bjlU4EyoVNg1gFFllEHEN2m0h2OLiwCLcBGAsYHQ/s1600/padoru.jpg'
+        // ,'https://1.bp.blogspot.com/-ZqlUt4kr-lU/XlhPHQxrhQI/AAAAAAAAFhs/i8MmM7nhu_gKFyBMLiCObokiCSZ4M5WiACLcBGAsYHQ/s1600/peron.png'
+        // ,'https://1.bp.blogspot.com/-1tgDYlvkK3E/XlhPHT7vcWI/AAAAAAAAFhw/VoXna4sQZRUBh1-ngK3ikgj5y4ZHgizYgCLcBGAsYHQ/s1600/puty.png'
+        // ,'https://1.bp.blogspot.com/-QQ6kc0p9sso/XlhPH93tChI/AAAAAAAAFh0/i4UhrgFOAMMew7ZEFsFVizXRmXyR20UIACLcBGAsYHQ/s1600/quesi.jpg'
+        // ,'https://1.bp.blogspot.com/-ZjH9eXj8Jlo/XlhPIDCOlcI/AAAAAAAAFh8/LarH0nB208wtifohy_jg78xe60PIbB2wQCLcBGAsYHQ/s1600/reimu.png'
+        // ,'https://1.bp.blogspot.com/-nYDwb4DGR3Y/XlhPILecOpI/AAAAAAAAFh4/ktjq-Q8NznEpXXeCU7H2ZELvDBwaJsQsACLcBGAsYHQ/s1600/rember.jpg'
+        // ,'https://1.bp.blogspot.com/-boKsO-PmCjU/XlhPI-zebxI/AAAAAAAAFiA/ET6DbgyXRP46qmGv20QXfaDFLdREE6nhwCLcBGAsYHQ/s1600/rhbmasro.jpg'
+        // ,'https://1.bp.blogspot.com/-XQfiuccxv78/XlhPJOffs6I/AAAAAAAAFiE/V-cjOsERmmsgaM4QVsaBNKLU04hpW0vvgCLcBGAsYHQ/s1600/seija.jpg'
+        // ,'https://1.bp.blogspot.com/-eIG2eDB_MYo/XlhPJdfJcyI/AAAAAAAAFiI/IQNaxvT-Zj8adiPC5hUGai7Oht0ZEGqMgCLcBGAsYHQ/s1600/tute.jpg'
+        // ,'https://1.bp.blogspot.com/-eKQlsV421o4/XlhPJntVLHI/AAAAAAAAFiM/Wui1F0NwvR0YoSBVxXBI4YKH58rzHoH0QCLcBGAsYHQ/s1600/unzan.jpg'
+        // ,'https://1.bp.blogspot.com/-FO8XvLovI4s/XlhPJ2RzvVI/AAAAAAAAFiQ/mkGdlzgoEmYdkDg_tD5RuQgekuuzRmqbwCLcBGAsYHQ/s1600/yukkuri.jpg'
+        // ,'https://1.bp.blogspot.com/-JSAfljVuOyo/XlhPKKU3_OI/AAAAAAAAFiU/MRhhTXxJTo42oNxpineguS4AxzuV3pIIACLcBGAsYHQ/s1600/zun.png'
+        // ,'https://1.bp.blogspot.com/-dRUlWYTBOek/XzC2oR4ccBI/AAAAAAAAGSo/DEAK97t5vVwZxTt8f0RGU6uEfAzL8icygCLcBGAsYHQ/s1600/pastel.png'
+        // ,'https://1.bp.blogspot.com/-PIfeVhlhOZU/X-X5PcLQcQI/AAAAAAAAGwk/esQV-moMeSohKyuUt2SLiCwbYumdtAYWgCLcBGAsYHQ/s16000/bankei.png'
+        // ,'https://1.bp.blogspot.com/-8upaD2OA9eA/X-lZnACHcEI/AAAAAAAAGxo/M6PDGOwEJEEvGhJJpLMBHl71PTzbfPM0ACLcBGAsYHQ/s1020/mochowo.png'
+        // ,'https://1.bp.blogspot.com/-eAEf7fM4ACw/X-gdE8HTXxI/AAAAAAAAGxM/FgKODI42qRcRmHaAieoZqnTXhm7qHdUHwCLcBGAsYHQ/s1020/pufo1.png'
+      ],
+      pajaro: "",
+      isNav: true
+    };
+  },
+  methods: {
+    changeHeader() {
+      var rand = Math.floor(Math.random() * Math.floor(this.parvada.length));
+      this.pajaro = this.parvada[rand];
+    }
+  },
+  created() {
+    this.changeHeader();
+  }
+};
+</script>
